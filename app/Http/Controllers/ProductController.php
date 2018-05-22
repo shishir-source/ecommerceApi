af49846586dc953a3eead9e3e487684fd04cd5af
shishir-source <shishir323@gmail.com>
2018-05-22 22:42:23 +0600

    Product destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -91,7 +91,7 @@ class ProductController extends Controller
         $request['detail'] = $request->description;
         unset($request['description']);
         $product->update($request->all());
-        
+
         return response([
             'data' => new ProductResource($product)
         ],Response::HTTP_CREATED);
@@ -105,6 +105,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response(null ,Response::HTTP_NO_CONTENT);
     }
 }
